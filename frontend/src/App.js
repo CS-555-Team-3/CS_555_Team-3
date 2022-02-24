b573fa241ed9b86475c13eff2f86f78c9181e742
coriwisniewski <43626560+coriwisniewski@users.noreply.github.com>
2022-02-23 22:36:09 -0500

    routing for game, organizing files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
-import AButton from "./components/Button";
+import Home from "./components/Home/Home"
+import Game from "./components/Game/Game";
+import GameOver from "./components/GameOver/GameOver";
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import './styles/App.css'
 
 function App() {
   return (
-    <div className="App">
-      <AButton />
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home/>}></Route>
+        <Route exact path='/game' element={<Game/>}></Route>
+        <Route exact path='/end' element={<GameOver/>}></Route>
+      </Routes>
+    </Router>
   );
 }
 
