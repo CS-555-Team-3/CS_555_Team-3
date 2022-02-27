131cda026bf5875cc593fc4da62a15c3ce16aa90
Franklin Shack <franklinshack@gmail.com>
2022-02-27 17:04:15 -0500

    added dragdrop components, now to debug

--- frontend/src/components/Game/Game.js
+++ frontend/src/components/Game/Game.js
@@ -2,6 +2,10 @@ import {Link} from 'react-router-dom';
 import PlayNote from './PlayNote';
 import '../../styles/Game.css';
 import NoteButton from './NoteButton';
+import Drag from 'react';
+import DropTarget from 'react';
+import itemDropped from 'react';
+
 
 export default function Game(props)
 {
@@ -14,32 +18,51 @@ export default function Game(props)
      *      -
      * */
 
+     function startDrag(ev) {
+        ev.dataTransfer.setData("drag-item", props.dataItem);
+      }
+
+      function dragOver(ev) {
+        ev.preventDefault();
+      }
+      
+      function drop(ev) {
+        const droppedItem = ev.dataTransfer.getData("drag-item");
+        if (droppedItem) {
+          props.onItemDropped(droppedItem);
+        }
+      }
+
     return (
         <div id="gameContainer">
             <div id="answerContainer">
                 <div className='resultRows'></div>
-                <div className='placement'>
-                    <button className='notes' disabled></button>
-                    <button className='notes' disabled></button>
-                    <button className='notes' disabled></button>
-                    <button className='notes' disabled></button>
-                    <button className='notes' disabled></button>
+                <DropTarget onItemDropped={itemDropped}>
+                    <div className='placement' onDragOver={dragOver} onDrop={drop}>
+                        <button className='notes' disabled></button>
+                        <button className='notes' disabled></button>
+                        <button className='notes' disabled></button>
+                        <button className='notes' disabled></button>
+                        <button className='notes' disabled></button>
+                    </div>
+                </DropTarget>
+            </div>
+            <Drag dataItem="item1">
+                <div id="noteContainer" draggable onDragStart={startDrag}>
+                        <NoteButton note="A_flat">Ab</NoteButton>
+                        <NoteButton note="A">A</NoteButton>
+                        <NoteButton note="B_flat">Bb</NoteButton>
+                        <NoteButton note="B">B</NoteButton>
+                        <NoteButton note="C">C</NoteButton>
+                        <NoteButton note="D_flat">Db</NoteButton>
+                        <NoteButton note="D">D</NoteButton>
+                        <NoteButton note="E_flat">Eb</NoteButton>
+                        <NoteButton note="E">E</NoteButton>
+                        <NoteButton note="F">F</NoteButton>
+                        <NoteButton note="G_flat">Gb</NoteButton>
+                        <NoteButton note="G">G</NoteButton>
                 </div>
-            </div>
-            <div id="noteContainer">
-                <NoteButton note="A_flat">Ab</NoteButton>
-                <NoteButton note="A">A</NoteButton>
-                <NoteButton note="B_flat">Bb</NoteButton>
-                <NoteButton note="B">B</NoteButton>
-                <NoteButton note="C">C</NoteButton>
-                <NoteButton note="D_flat">Db</NoteButton>
-                <NoteButton note="D">D</NoteButton>
-                <NoteButton note="E_flat">Eb</NoteButton>
-                <NoteButton note="E">E</NoteButton>
-                <NoteButton note="F">F</NoteButton>
-                <NoteButton note="G_flat">Gb</NoteButton>
-                <NoteButton note="G">G</NoteButton>
-            </div>
+            </Drag>
             <Link to="/end">End Game</Link>
         </div>
     );
