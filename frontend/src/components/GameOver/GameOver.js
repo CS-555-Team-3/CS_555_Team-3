ad86e75e3b194b467de1396749294bc26d7ada4a
Tyler Soiferman <96785434+tsoiferm@users.noreply.github.com>
2022-02-27 13:48:42 -0500

    end of game screen v1

--- frontend/src/components/GameOver/GameOver.js
+++ frontend/src/components/GameOver/GameOver.js
@@ -1,11 +1,31 @@
 import {Link} from 'react-router-dom';
+import React, { Component } from "react"
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 export default function GameOver(props)
 {
-    //Todo - personal leaderboard, save score, replay game
+   copyCodeToClipboard = () => {
+        const el = this.textArea
+        el.select()
+        document.execCommand("copy")
+      }
+   
     return(
-        <>
-            <h3>End of game content - Tyler</h3>
-        </>
+        <div>
+            <>
+                <Link to="/game">Play Again!</Link>
+            </>
+            <div>
+                <textarea
+                    ref={(textarea) => this.textArea = textarea}
+                    value="Your score is: 0 | Number of Tries: (# of tries)"
+                />
+            </div>
+            <div>
+                <button onClick={() => this.copyCodeToClipboard()}>
+                    Copy to Clipboard
+                </button>
+            </div>
+         </div>
     );
 }
