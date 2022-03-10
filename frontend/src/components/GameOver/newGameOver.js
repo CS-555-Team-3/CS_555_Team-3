8424977cb3343a9c8d5731a5d6474fbb13e74f52
Tyler Soiferman <96785434+tsoiferm@users.noreply.github.com>
2022-03-10 13:02:52 -0500

    Add files via upload

---
+++ frontend/src/components/GameOver/newGameOver.js
@@ -0,0 +1,33 @@
+import "./styles.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
+
+export default function GameOver(props) {
+    state = {
+        value: '',
+        copied: false,
+    };
+    
+    render() {
+        return (
+            <div>
+                <input value= {state.value}
+                  onChange={({target: {value}}) => setState({value, copied: false})} />
+                
+                <CopyToClipboard text={state.value}
+                  onCopy={() => setState({copied: true})}>
+                  <span> Copy to clipboard with span</span>
+                </CopyToClipboard>
+
+
+                <CopyToClipboard text={state.value} 
+                  onCopy={() => setState({copied: true})}>
+                  <button>Copy to clipboard with button</button>
+                </CopyToClipboard>
+
+                {state.copied ? <span style={{color: 'red'}}>Copied!</span> : null}
+            </div>
+        );
+    }
+}
