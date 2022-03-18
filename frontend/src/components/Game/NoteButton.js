7fecd599e74d9f8ff747ce9aa6c5ba0526268545
Tyler Soiferman <96785434+tsoiferm@users.noreply.github.com>
2022-03-18 13:58:20 -0400

    Update NoteButton.js

--- frontend/src/components/Game/NoteButton.js
+++ frontend/src/components/Game/NoteButton.js
@@ -5,12 +5,17 @@ export default function NoteButton(props)
 {
     let note = props.note;
     let noteName = props.children;
+    let the_icon;
+    if (mode == true) {
+        the_icon = "https://static.vecteezy.com/system/resources/previews/000/546/381/original/music-notes-vector-icon.jpg"
+    }
 
 
     return(
         <div className={`noteButton ${note}`}>
             <SetNote note={note}>
             {noteName}
+            {the_icon}
             </SetNote>
             <PlayNote note={note}>
             </PlayNote>
