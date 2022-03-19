884208065f289074d5556f3e269f40bf882f884b
jay-trash <jay@trash.xn--6frz82g>
2022-03-19 18:39:42 -0400

    fix names

--- frontend/src/components/Game/AddScore.js
+++ frontend/src/components/Game/AddScore.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react"; 
 
-export default function addScore(val) {
+export default function AddScore(val) {
     const [scores, setScores] = useState([]);
 
     //Load scores from local storage on pageload
