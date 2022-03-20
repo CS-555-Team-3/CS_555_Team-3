6f335cd982cb133d3bc8e4e0ad59d61c74504182
jay-trash <jay@trash.xn--6frz82g>
2022-03-19 20:30:09 -0400

    print scores bugfix

--- frontend/src/components/Game/PrintScores.js
+++ frontend/src/components/Game/PrintScores.js
@@ -3,13 +3,21 @@ import { useState, useEffect } from "react";
 export default function PrintScores(val) {
     const [scores, setScores] = useState([]);
 
-    //Load scores from local storage on pageload
-    useEffect(() => {
-            const json = localStorage.getItem("scores");
-            const savedScores = JSON.parse(json);
-            if (savedScores) { setScores(savedScores); }
+       //Load scores from local storage on pageload
+       useEffect(() => {
+        const json = localStorage.getItem("scores");
+        const savedScores = JSON.parse(json);
+        if (savedScores) { setScores(savedScores); }
         }, 
         []
+    );
+
+    //Save scores to local storage when changed
+    useEffect(() => { 
+        const json = JSON.stringify(scores);
+        localStorage.setItem("scores", json);
+        }, 
+        [scores]
     );
     
     //Gets top 5 scores from local storage
