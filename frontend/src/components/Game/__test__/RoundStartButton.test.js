6cef21c0dabd885ce70a8f75b41a0c11f0fa9538
洪啟睿 <y6602016@gmail.com>
2022-03-23 20:31:33 -0400

    fix unit test

--- frontend/src/components/Game/__test__/RoundStartButton.test.js
+++ frontend/src/components/Game/__test__/RoundStartButton.test.js
@@ -1,9 +1,35 @@
-import {render, cleanup} from "@testing-library/react";
 import RoundStartButton from "../RoundStartButton";
+import { render, screen, cleanup, waitFor } from '@testing-library/react';
+import userEvent from "@testing-library/user-event";
+import React from 'react';
+import { act } from 'react-dom/test-utils';
+import { ExpandCircleDownTwoTone, JoinLeftSharp } from "@mui/icons-material";
 
 afterEach(cleanup);
+afterEach(()=>
+{
+    jest.clearAllMocks();
+})
 
-test('renders', () => {
-    const div = document.createElement("div")
-    render(<RoundStartButton />, div);
-});
+beforeEach(()=> jest.setTimeout(30000));
+
+describe('Does click highlight note', () =>
+{
+    test('Highlight', async () =>
+    {
+        const audio = new Audio('/sound_notes/A.wav');
+        const onClick = jest.fn();
+        render(
+            <RoundStartButton value={audio} onClick={onClick}></RoundStartButton>
+        );
+        let button = document.getElementsByClassName('roundStart');
+        act(()=>
+        Array.from(button).forEach((note)=>
+        {
+            userEvent.click(note);
+        }));
+        
+        // if click roundStart, highlight function is called
+        expect(onClick).toHaveBeenCalled();
+    })
+})
