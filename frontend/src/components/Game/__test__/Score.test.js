fa518ff254a15f7717ab7d06f7407b2897da2917
jay-trash <jay@trash.xn--6frz82g>
2022-04-03 18:42:31 -0400

    tests

--- frontend/src/components/Game/__test__/Score.test.js
+++ frontend/src/components/Game/__test__/Score.test.js
@@ -1,9 +1,29 @@
-import {render, cleanup} from "@testing-library/react";
+import {render, cleanup, userEvent} from "@testing-library/react";
 import PrintScores from "../PrintScores";
+import ResultButton from "../ResultButton";
 
 afterEach(cleanup);
 
 test('renders', () => {
     const div = document.createElement("div")
     render(<PrintScores />, div);
+});
+
+test('score adds', () => {
+    render(<ResultButton />,<PrintScores />)
+    let scores = document.getElementsByClassName('scoreGroup')
+    Array.from(scores).forEach((score)=> {
+        userEvent.click(getElementById('resultButton'));
+        expect(score==0)
+    })
+});
+
+test('time and difficulty present', () => {
+    render(<ResultButton />,<PrintScores />)
+    let scores = document.getElementsByClassName('scoreGroup')
+    Array.from(scores).forEach((score)=> {
+        userEvent.click(getElementById('resultButton'));
+        expect(score.time>=0 && score.time <= 0.1)
+        expect(score.difficulty.length() == 1)
+    })
 });
