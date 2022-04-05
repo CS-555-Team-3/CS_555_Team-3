c1cdfb9bb5173e4ad36316cc8d3980872eba433a
SexyHamilton <wanghanyu329@gmail.com>
2022-04-05 17:02:13 -0400

    fix test file

--- frontend/src/components/Game/__test__/ResultButton.test.js
+++ frontend/src/components/Game/__test__/ResultButton.test.js
@@ -1,19 +1,20 @@
 import { render, screen, cleanup } from "@testing-library/react";
-import NoteButton from '../NoteButton.js';
 import ResultButton from "../ResultButton";
 import userEvent from '@testing-library/user-event';
 import React from "react";
-import { green } from "@mui/material/colors";
+
 
 afterEach(cleanup);
 afterEach(() => {
   jest.clearAllMocks();
 });
 
-describe("Does box lighted up", () => {
+describe("Are boxes painted correctly after clicking resultButton", () => {
   test("submit answer and box is painted", async () => {
+    // ans order = ["A", "B", "C", "D"]
+    // user choice = ["A", "C", "G", "D"]
+    // expect color = ["green", "yellow", "red", "green"]
     const order = ["A", "B", "C", "D"];
-    const userChoice = ["A", "C", "G", "D"];
     const boxes = ["first", "second", "third", "fourth"];
     
     render(
@@ -21,39 +22,24 @@ describe("Does box lighted up", () => {
                 <div id="answerContainer">
                     <div className='resultRows'></div>
                     <div className='placement'>
-                        <button id={boxes[0]} className='notes'  disabled> A</button>
-                        <button id={boxes[1]} className='notes' disabled> B</button>
-                        <button id={boxes[2]} className='notes' disabled> C</button>
-                        <button id={boxes[3]} className='notes' disabled> D </button>
+                        <button id={boxes[0]} className='notes'  disabled >A</button>
+                        <button id={boxes[1]} className='notes' disabled>C</button>
+                        <button id={boxes[2]} className='notes' disabled>G</button>
+                        <button id={boxes[3]} className='notes' disabled>D</button>
                         
                     </div>
                 </div>
                 <div>
-                <ResultButton order={order} userChoice = {userChoice} boxes = {boxes}  >Submit Answer </ResultButton>
+                <ResultButton order={order}  >Submit Answer</ResultButton>
                 </div>
-                {/* <div>
-                <div id="noteContainer">    
-                    <NoteButton order={order} note="A_flat">Ab</NoteButton>
-                    <NoteButton order={order} note="A">A</NoteButton>
-                    <NoteButton order={order} note="B_flat">Bb</NoteButton>      
-                    <NoteButton order={order} note="B">B</NoteButton>
-                    <NoteButton order={order} note="C">C</NoteButton>
-                    <NoteButton order={order} note="D_flat">Db</NoteButton>
-                    <NoteButton order={order} note="D">D</NoteButton>
-                    <NoteButton order={order} note="E_flat">Eb</NoteButton>
-                    <NoteButton order={order} note="E">E</NoteButton>
-                    <NoteButton order={order} note="F">F</NoteButton>
-                    <NoteButton order={order} note="G_flat">Gb</NoteButton>
-                    <NoteButton order={order} note="G">G</NoteButton>
-                </div>
-                </div> */}
+
             </div>
     
     );
     userEvent.click(screen.getByText('Submit Answer'));
-    expect(screen.getAllByText('A').length).toBe(1);
-    expect(screen.getAllByText('C').length).toBe(1);
-    expect(screen.queryByText('G')).not.toBeInTheDocument();
-    expect(screen.getAllByText('D').length).toBe(1);
+    expect(document.getElementById(boxes[0]).style.backgroundColor).toBe("green");
+    expect(document.getElementById(boxes[1]).style.backgroundColor).toBe("yellow");
+    expect(document.getElementById(boxes[2]).style.backgroundColor).toBe("red");
+    expect(document.getElementById(boxes[3]).style.backgroundColor).toBe("green");
   });
 });
