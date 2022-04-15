4cbdb2fcdb91f729683f6d1369436cdb5542f949
Tyler Soiferman <twsoiferman@outlook.com>
2022-04-14 21:24:37 -0400

    cleaned up

--- frontend/src/components/Game/RoundStartButton.js
+++ frontend/src/components/Game/RoundStartButton.js
@@ -56,39 +56,6 @@ export default function RoundStartButton(props) {
 
     };
 
-    const Timer = () => {
-        const {initialMinute = 0,initialSeconds = 0} = props;
-        const [ minutes, setMinutes ] = useState(initialMinute);
-        const [seconds, setSeconds ] =  useState(initialSeconds);
-        useEffect(()=>{
-        let myInterval = setInterval(() => {
-                if (seconds > 0) {
-                    setSeconds(seconds - 1);
-                }
-                if (seconds === 0) {
-                    if (minutes === 0) {
-                        clearInterval(myInterval)
-                    } else {
-                        setMinutes(minutes - 1);
-                        setSeconds(59);
-                    }
-                } 
-            }, 1000)
-            return ()=> {
-                clearInterval(myInterval);
-              };
-        });
-    
-        return (
-            <div>
-            { minutes === 0 && seconds === 0
-                ? null
-                : <h1> {minutes}:{seconds < 10 ?  `0${seconds}` : seconds}</h1> 
-            }
-            </div>
-        )
-    };
-
     return (
         <div>
             <div>
