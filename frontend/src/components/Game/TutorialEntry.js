a16505431f0538902c6e24e73245f3194022ac32
Franklin Shack <franklinshack@gmail.com>
2022-04-21 20:52:45 -0400

    Updated tutorial

--- frontend/src/components/Game/TutorialEntry.js
+++ frontend/src/components/Game/TutorialEntry.js
@@ -7,11 +7,15 @@ export default function TutorialEntry() {
                 HOW TO PLAY SOUNDLE <Button id="tutorialExit" variant="text" onClick={() => {document.getElementById("tutorialOverlayBG").style.display = "none"; }}>Close</Button>
                 <div className="tutorialText">
                     <p>You have six attempts to guess the given chord.</p>
-                    <p>Each guess must contain five notes, hit the "Submit Answer" button to submit a guess.</p>
+                    <p>Each guess must contain a certain number of notes depending on the difficulty. Hit the "Submit Answer" button to submit a guess.</p>
                     <p>After each attempt, you will be given feedback in the form of the color of the tiles.  The colors will change based on whether each note is in the chord, and if it is in the correct position.</p>
                     <p>Examples:</p>
-                    <img src={require('./img/onecorrect.png')}></img>
-                    <p>A is in the chord and is in the correct position.</p>
+                    <img src={require('./img/3R1Y.png')}></img>
+                    <p>One guessed note is in the chord but not in the.</p>
+                    <img src={require('./img/1G2Y1R.png')}></img>
+                    <p>One guessed note is in the chord and in the correct position, while two are in the chord but not in the correct positions.</p>
+                    <img src={require('./img/4G.png')}></img>
+                    <p>All notes are in the correct positions.</p>
                 </div>
             </div>
         </div>
